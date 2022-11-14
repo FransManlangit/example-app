6dd5b16b61d1c66fdd95c10f4cf111e7810d3af9
FransManlangit <fransmanlangit11@gmail.com>
2022-11-14 16:18:38 +0800

    new changes

---
+++ app/Models/Customer.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+// class customer extends Model
+// {
+//     use HasFactory;
+
+//     protected $table = "customers";
+
+//     // protected $guarded = ["id"];
+
+//     protected $primaryKey = "customer_id";
+
+//     protected $fillable = ['title', 'fname', 'lname','addressline','town','zipcode','phone','creditlimit','level','user_id'];
+
+//     public $timestamps = false;
+// }
+
+// class Customer extends Model
+// {
+//     use HasFactory;
+//     public $timestamps = false;
+//     public $primaryKey = 'customer_id';
+
+//     protected $guarded = ['customer_id'];
+//     protected $fillable = ['title', 'fname','lname','addressline','town', 'zipcode','phone','creditlimit','level'];
+// }
+
+class Customer extends Model
+{
+    use HasFactory;
+    public $table = 'customer';
+    public $primaryKey = 'customer_id';
+    public $timestamps = false;
+    protected $guarded = ['customer_id'];
+    
+
+    protected $fillable = ['fname','lname',
+        'title','addressline','town','zipcode',
+        'phone','email','user_id'
+    ];
+
+     public function orders(){
+
+        return $this->hasMany('App\Models\Order','customer_id');
+
+    }
+}
